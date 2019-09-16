13030cecfddc19d2824d89523ae0722dbab6d77a
Ozkok Ozan <oozkok@S-204-10>
2019-09-16 10:57:38 +0200

    Troisieme

--- SalleCinema.cs
+++ SalleCinema.cs
@@ -8,5 +8,41 @@ namespace TPCinema
 {
     class SalleCinema
     {
+        private string titre;
+        private int nbPlaces;
+        private double tarifN;
+        private int nbPlaceTarifN = 0;
+        private int nbPlaceTarifR = 0;
+
+        public SalleCinema(string leTitre, int lesPlaces, double tarifNormal, int leNbPlaceTarifN, int leNbPlaceTarifR)
+        {
+            this.Titre = leTitre;
+            this.NbPlaces = lesPlaces;
+            this.TarifN = tarifNormal;
+            this.NbPlaceTarifN = leNbPlaceTarifN;
+            this.NbPlaceTarifR = leNbPlaceTarifR;
+        }
+
+        public string Titre
+        {
+            get => titre; set => titre = value;
+        }
+        public int NbPlaces
+        {
+            get => nbPlaces; set => nbPlaces = value;
+        }
+        public double TarifN
+        {
+            get => tarifN; set => tarifN = value;
+        }
+        public int NbPlaceTarifN
+        {
+            get => nbPlaceTarifN; set => nbPlaceTarifN = value;
+        }
+        public int NbPlaceTarifR
+        {
+            get => nbPlaceTarifR; set => nbPlaceTarifR = value;
+        }
+        
     }
 }
